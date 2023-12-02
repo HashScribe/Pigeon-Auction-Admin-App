7d433ce021a5e42527b7eddf4c8811514940df8b
Ruhaim <ruhaim860@gmail.com>
2023-12-02 12:20:44 +0530

    initialize firebase

---
+++ firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANR6FSftyM4uec41OAUNMsMv_G9xz1jjQ",
+  authDomain: "pigeon-auction-b0c45.firebaseapp.com",
+  projectId: "pigeon-auction-b0c45",
+  storageBucket: "pigeon-auction-b0c45.appspot.com",
+  messagingSenderId: "359838590324",
+  appId: "1:359838590324:web:20f07fc66e64efdb24f821",
+  measurementId: "G-HEFW17SQYV",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export { app };
